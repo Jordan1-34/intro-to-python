be1111c9e5e61963d0f6cd6819fb9ea02a0f86b3
Jordan1-34 <131589592+Jordan1-34@users.noreply.github.com>
2024-08-02 21:48:50 +0100

    Update 013_add_two.py

--- 013_add_two.py
+++ 013_add_two.py
@@ -16,7 +16,11 @@ from lib.helpers import check_that_these_are_equal
 # Here's how: https://vimeo.com/956351893/f5a80a1c1b
 
 # YOUR FUNCTION GOES BELOW THIS LINE
+print("Function: add_two")
 
+check_that_these_are_equal(
+  add_two(6),
+  
 
 
 # YOUR FUNCTION GOES ABOVE THIS LINE
